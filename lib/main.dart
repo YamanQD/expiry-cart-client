8fe77ec9b4d70af8a4cb7073d599120151ee9033
Yaman Qassas <yaman102011@gmail.com>
2022-01-04 23:14:30 +0200

    Clean up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
-import 'package:expiry_cart/Screens/categories_list_page.dart';
-import 'package:expiry_cart/Screens/product_page.dart';
-import 'package:expiry_cart/categories_helper/utils.dart';
 import 'package:expiry_cart/Screens/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:expiry_cart/Screens/welcome_page.dart';
 
 void main() => runApp(
       MaterialApp(
-        // AddProductPage(),
-        // EditProductPage(),
-        // ProductsPage(selected: Utils.getCategories()[2]),
-        home: // ProductsPage(selected: Utils.getCategories()[2]),
-            // const WelcomePage(),
-            // Categories(),
-            // DetailsPage(productDetail: Utils.get()[0].subCategories[0]),
-            //  DetailsPage(productDetail: Utils.get()[0].subCategories[0]),
-            SplashPage(
+        home: SplashPage(
           duration: 5,
           navigateToPage: const WelcomePage(),
         ),
